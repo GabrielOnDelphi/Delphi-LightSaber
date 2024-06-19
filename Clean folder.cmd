1ed6f5cbb3e16123f24c913871cf09150a4d897f
Gabriel Moraru <cubicDesign@gmail.com>
2024-06-19 20:35:22 +0200

    extra files

---
+++ Clean folder.cmd
@@ -0,0 +1,17 @@
+echo off
+prompt $
+cls
+
+del /s *.exe
+del /s *.map
+del /s *.dsm
+del /s *.ddp
+del /s *.bak
+del /s *.drc
+del /s *.local
+del /s *.identcache
+del /s *.stat
+del /s *.~dsk
+del /s *.dproj.local
+
+delay 500 s b
