f88553dbeecd2712b574bc18bc2411ac1414ead8
Gabriel Moraru <cubicDesign@gmail.com>
2025-04-19 16:54:44 +0200

    improved demo

--- Demo/FMX - Demo Message boxes/MainForm.pas
+++ Demo/FMX - Demo Message boxes/MainForm.pas
@@ -12,15 +12,21 @@ uses
 TYPE
   TForm1 = class(TLightForm)
     btnFmxDialogs: TButton;
-    Button2: TButton;
+    btnMessageInfo: TButton;
     Button3: TButton;
     btnQuestion: TButton;
     btnFmxDialogs2: TButton;
+    btnInputQuery1: TButton;
+    btnInputQuery3: TButton;
+    btnInputQuery2: TButton;
     procedure btnFmxDialogsClick(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
+    procedure btnMessageInfoClick(Sender: TObject);
     procedure Button3Click(Sender: TObject);
     procedure btnQuestionClick(Sender: TObject);
     procedure btnFmxDialogs2Click(Sender: TObject);
+    procedure btnInputQuery1Click(Sender: TObject);
+    procedure btnInputQuery2Click(Sender: TObject);
+    procedure btnInputQuery3Click(Sender: TObject);
   private
     procedure DoSomething;
   public
@@ -49,15 +55,12 @@ end;
 
 
 
-
-
-
-
 // Synchronicity: Uses TDialogServiceAsync on Android, and TDialogServiceSync on the other platforms
 // Custom caption: No
 // Deprecated: No
 // Icons: No
 // Ding: No
+// Has X button: Yes
 procedure TForm1.btnFmxDialogsClick(Sender: TObject);
 begin
   Caption:= '';
@@ -93,6 +96,8 @@ end;
 // Custom caption: No
 // Icons: Yes
 // Ding: Yes
+// Has X button: Yes
+
 // You can force TDialogService.PreferredMode := TPreferredMode.Sync on mobile to simulate synchronous behavior, but not recommended
 procedure TForm1.Button3Click(Sender: TObject);
 begin
@@ -107,6 +112,8 @@ begin
 end;
 
 
+
+// Has X button: Yes BUT it is disabled (so it does nothing)
 procedure TForm1.btnQuestionClick(Sender: TObject);
 begin
   Caption:= '';
@@ -130,7 +137,8 @@ end;
 // Custom caption: No
 // Icons: No
 // Ding: Yes
-procedure TForm1.Button2Click(Sender: TObject);
+// Has X button: No
+procedure TForm1.btnMessageInfoClick(Sender: TObject);
 begin
   Caption:= '';
   MessageInfo('MessageInfo', 'No caption?');
@@ -139,6 +147,71 @@ begin
   //MessageWarning('Hello info', 'Caption');
   //MessageError('Hello info', 'Caption');
   //MessageErrorSubmit('Hello info', 'Caption');
+end;
+
+
+
+// TDialogService.InputQuery() is a method in the FireMonkey (FMX) framework that provides a cross-platform way to prompt the user for input.
+// InputQuery can work synchronously or asynchronously depending on the preferred mode.
+// InputQuery internally calls TDialogServiceAsync.InputQuery or TDialogServiceSync.InputQuery.
+// Has X button: Yes BUT it does nothing!
+procedure TForm1.btnInputQuery1Click(Sender: TObject);
+begin
+  FMX.DialogService.TDialogService.PreferredMode:= TDialogService.TPreferredMode.Async;
+
+  TDialogService.InputQuery(
+      'Enter Your Name',          // Dialog title
+      ['Name:'],                  // Array of prompts for input fields
+      [''],                       // Array of default values
+      procedure(const AResult: TModalResult; const AValues: array of string)
+      begin
+        if AResult = mrOk
+        then Caption:= 'Hello, ' + AValues[0]
+        else Caption:= 'User cancelled';
+      end
+    );
+end;
+
+
+// InputQuery supports multiple input fields. For instance, to collect a first and last name:
+procedure TForm1.btnInputQuery2Click(Sender: TObject);
+begin
+  FMX.DialogService.TDialogService.PreferredMode:= TDialogService.TPreferredMode.Async;
+
+  TDialogService.InputQuery(
+    'Enter Your Details',
+    ['First Name:', 'Last Name:'],
+    ['', ''],
+    procedure(const AResult: TModalResult; const AValues: array of string)
+    begin
+      if AResult = mrOk
+      then Caption := 'Hello, ' + AValues[0] + ' ' + AValues[1];
+    end
+  );
+end;
+
+
+// Wrong usage example.
+// Because of the asynchronicity, the Caption will be initially set to 'John Doe'. Only after the user enters its name, the Caption will show correct data!
+procedure TForm1.btnInputQuery3Click(Sender: TObject);
+VAR UserName: string; // Scope Capture: The anonymous callback can capture variables from the surrounding scope, which is useful for maintaining context:
+begin
+  FMX.DialogService.TDialogService.PreferredMode:= TDialogService.TPreferredMode.Async;
+
+  UserName:= 'John Doe';
+  TDialogService.InputQuery(
+    'Enter Your Details',
+    ['First Name:', 'Last Name:'],
+    ['', ''],
+    procedure(const AResult: TModalResult; const AValues: array of string)
+    begin
+      if AResult = mrOk
+      then Caption := 'Hello, ' + AValues[0] + ' ' + AValues[1];
+      UserName:= AValues[0];
+    end
+  );
+
+  Caption:= UserName; // This is executed BEFORE the user has a chance to enter its name!
 end;
 
 
