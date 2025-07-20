c8c057ced37a44ced65faa1661daaeb13800745e
Gabriel Moraru <cubicDesign@gmail.com>
2025-07-20 11:09:12 +0200

    new unit

---
+++ FrameVCL/LightVcl.Common.SystemConsole.pas
@@ -0,0 +1,35 @@
+﻿UNIT LightVcl.Common.SystemConsole;
+
+{=============================================================================================================
+   2025.05
+   www.GabrielMoraru.com
+=============================================================================================================}
+
+INTERFACE
+USES
+   Winapi.Windows,
+   System.Classes, System.SysUtils, Vcl.Graphics;
+
+
+procedure SetConsoleColor(AColor: TColor);
+
+
+IMPLEMENTATION
+
+
+// Change the color of console output.                                                                        https://stackoverflow.com/questions/57980596/change-text-color-in-delphi-console-application
+procedure SetConsoleColor(AColor: TColor);
+begin
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED or FOREGROUND_GREEN or FOREGROUND_BLUE);
+  case AColor of
+    clWhite:  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED or FOREGROUND_GREEN or FOREGROUND_BLUE);
+    clRed:    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED or FOREGROUND_INTENSITY);
+    clGreen:  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN or FOREGROUND_INTENSITY);
+    clBlue:   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_BLUE or FOREGROUND_INTENSITY);
+    clMaroon: SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN or FOREGROUND_RED or FOREGROUND_INTENSITY);
+    clPurple: SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED or FOREGROUND_BLUE or FOREGROUND_INTENSITY);
+    clAqua  : SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN or FOREGROUND_BLUE or FOREGROUND_INTENSITY);
+  end;
+end;
+
+end.
