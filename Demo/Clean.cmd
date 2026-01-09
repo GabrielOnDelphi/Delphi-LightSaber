2e07847565bc60ae7c9c46881eecf2f54ef4151d
Gabriel Moraru <cubicDesign@gmail.com>
2026-01-09 18:48:50 +0100

    clean cmd

---
+++ Demo/Clean.cmd
@@ -0,0 +1,23 @@
+echo off
+prompt $
+cls
+
+del /s *.~*
+del /s *.dcu
+del /s *.map
+del /s *.exe
+del /s *.rsm
+
+del /s *.bin
+del /s *.json
+del /s *.o
+del /s *.so
+del /s *.apk
+del /s Android*.xml
+del /s classe*.dex
+del /s PackagedResources.zip
+del /s app_process64
+del /s linker64
+del /s *.apk.idsig
+
+delay 500 s b
