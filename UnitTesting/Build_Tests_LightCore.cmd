41542d2773d9ed1a004680fb2c4db6ec2a592ef1
Gabriel Moraru <cubicDesign@gmail.com>
2026-02-11 22:38:44 +0100

    Update Build_Tests_LightCore.cmd

--- UnitTesting/Build_Tests_LightCore.cmd
+++ UnitTesting/Build_Tests_LightCore.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "c:\Delphi\Delphi 13\bin\rsvars.bat"
 REM Build without TESTINSIGHT to enable console output
-"c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "c:\Projects\LightSaber\Tests\Tests_LightCore.dproj" /t:Build /p:platform=Win32 /p:Configuration=Debug /p:DCC_Define="DEBUG"
+"c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "c:\Projects\LightSaber\UnitTesting\Tests_LightCore.dproj" /t:Build /p:platform=Win32 /p:Configuration=Debug /p:DCC_Define="DEBUG"
