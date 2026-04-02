3c34b55d0e1bf9ca2aeeaf8ac56a884609c50bcd
Gabriel Moraru <cubicDesign@gmail.com>
2026-04-02 20:45:47 +0200

    Update LightFmx.Common.Styles.pas

--- FrameFMX/LightFmx.Common.Styles.pas
+++ FrameFMX/LightFmx.Common.Styles.pas
@@ -24,10 +24,16 @@ function GetThemeTextColor: TAlphaColor; overload;
   Uses FMX.Utils.Luminance (ITU-R BT.709) on the style's background color. }
 function IsDarkStyle: Boolean;
 
+{ Generates a palette of colors derived from BaseColor, adjusted for theme visibility.
+  Light themes: darker shades (visible on light backgrounds).
+  Dark themes: lighter tints (visible on dark backgrounds).
+  Falls back to neutral grays if BaseColor is black/near-black. }
+function GenerateThemePalette(BaseColor: TAlphaColor; IsDark: Boolean): TArray<TAlphaColor>;
+
 IMPLEMENTATION
 
 USES
-   FMX.Objects, FMX.Utils;
+   FMX.Objects, FMX.Utils, System.UIConsts;
 
 
 { Probes the style file to check if it's compatible with the current platform.
@@ -139,6 +145,48 @@ begin
   BgObj:= ActiveStyle.FindStyleResource('background');
   if (BgObj <> NIL) AND (BgObj is TBrushObject)
   then EXIT(Luminance(TBrushObject(BgObj).Brush.Color) < 0.5);
+end;
+
+
+
+{-------------------------------------------------------------------------------------------------------------
+   PALETTE GENERATION
+-------------------------------------------------------------------------------------------------------------}
+function GenerateThemePalette(BaseColor: TAlphaColor; IsDark: Boolean): TArray<TAlphaColor>;
+CONST
+  STEPS = 10;
+VAR
+  H, S, L: Single;
+  MinL, MaxL, StepSize: Single;
+begin
+  RGBtoHSL(BaseColor, H, S, L);
+
+  // Black/near-black: generate neutral grays since HSL variations produce nothing useful
+  if (L < 0.05) AND (S < 0.1) then
+    begin
+      SetLength(Result, 8);
+      for VAR i:= 0 to 7 do
+        begin
+          if IsDark
+          then Result[i]:= HSLtoRGB(0, 0, 0.30 + i * 0.08)
+          else Result[i]:= HSLtoRGB(0, 0, 0.15 + i * 0.08);
+          TAlphaColorRec(Result[i]).A:= $FF;
+        end;
+      EXIT;
+    end;
+
+  // Normal: sweep lightness across a range appropriate for the theme
+  if IsDark
+  then begin MinL:= 0.35; MaxL:= 0.85; end
+  else begin MinL:= 0.15; MaxL:= 0.65; end;
+
+  StepSize:= (MaxL - MinL) / (STEPS - 1);
+  SetLength(Result, STEPS);
+  for VAR i:= 0 to STEPS - 1 do
+    begin
+      Result[i]:= HSLtoRGB(H, S, MinL + i * StepSize);
+      TAlphaColorRec(Result[i]).A:= $FF;
+    end;
 end;
 
 
