28d98ce4cd1f089f4c8a8e64c2c2782eab060148
Gabriel Moraru <cubicDesign@gmail.com>
2026-05-26 22:30:04 +0200

    new updater

---
+++ FrameVCL/FormUpdaterNotifier.pas
@@ -0,0 +1,363 @@
+UNIT FormUpdaterNotifier;
+{-------------------------------------------------------------------------------------------------------------
+   2026.02
+   www.GabrielMoraru.com
+
+   Show info and updates about a commercial program.
+
+-------------------------------------------------------------------------------------------------------------
+
+   THIS FORM IS USED BY MULTIPLE PROGRAMS.
+   Do not localize it for a specific program!
+
+   How to use it:
+       Create the form with ShowUpdater(False);
+       Call
+         TUpdater.CheckForNewsNow;
+         TUpdater.CheckForNewsTry;
+         TUpdater.CheckForNewsDelay(Seconds);  // automatically check for news 9 seconds after program startup
+
+   Note:
+        You don't have to manually created this form. The TUpdater object will create the form for you if there are new, or if there is a connection error.
+
+   See "ciUpdater.pas" for details.
+-------------------------------------------------------------------------------------------------------------}
+
+//ToDo: Add the news to a list of news. This way the user will also see previous news, in case he missed them. Also allow me to clear that list.
+
+INTERFACE
+{$DENYPACKAGEUNIT ON} {Prevents unit from being placed in a package. https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Packages_(Delphi)#Naming_packages }
+
+USES
+  System.SysUtils, System.Classes, Vcl.Forms, LightVcl.Visual.AppDataForm,Vcl.StdCtrls, Vcl.Controls, Vcl.ExtCtrls, Vcl.ComCtrls,
+  InternetLabel,
+  LightCore, ciUpdater, LightVcl.Visual.RichLog, LightVcl.Visual.RichLogTrack, FormUpdaterSettings, FormUpdaterRecEditor;
+
+CONST
+  UpdaterDemoURL = 'https://www.GabrielMoraru.com/uploads/OnlineNews_Template.ini'; { For demo purposes }
+
+TYPE
+  TFrmUpdater = class(TLightForm)
+    Log              : TRichLog;
+    PageCtrl         : TPageControl;
+    btnBinFile       : TButton;
+    btnCheckDelay    : TButton;
+    btnCheckManually : TButton;
+    btnCheckToday    : TButton;
+    btnIsTimeToCheck : TButton;
+    btnNewVersFound  : TButton;
+    btnSettings      : TButton;
+    Button1          : TButton;
+    inetWhatsNew     : TInternetLabel;
+    lblDownload      : TInternetLabel;
+    lblConnectError  : TLabel;
+    lblStatus        : TLabel;
+    lblVersion       : TLabel;
+    Panel1           : TPanel;
+    Panel2           : TPanel;
+    Panel3           : TPanel;
+    pnlBottom        : TPanel;
+    tabDemo          : TTabSheet;
+    tabNews          : TTabSheet;
+    tabRecEditor     : TTabSheet;
+    LogVerb          : TRichLogTrckbr;
+    procedure btnBinFileClick       (Sender: TObject);
+    procedure btnCheckDelayClick    (Sender: TObject);
+    procedure btnCheckManuallyClick (Sender: TObject);
+    procedure btnCheckTodayClick    (Sender: TObject);
+    procedure btnIsTimeToCheckClick (Sender: TObject);
+    procedure btnNewVersFoundClick  (Sender: TObject);
+    procedure btnSettingsClick      (Sender: TObject);
+    procedure OnConnectError        (Sender: TObject; Msg: string);
+    procedure OnHasNews             (Sender: TObject);
+    procedure OnNoNews              (Sender: TObject);
+    procedure OnUpdateEnd           (Sender: TObject);
+    procedure OnUpdateStart         (Sender: TObject);
+    procedure FormCreate            (Sender: TObject);
+    procedure FormDestroy           (Sender: TObject);
+    procedure FormClose             (Sender: TObject; var Action: TCloseAction);
+  private
+    procedure PopulateNews;
+  public
+    procedure ShowDemoTab;
+ end;
+
+
+IMPLEMENTATION  {$R *.DFM}
+
+USES
+   LightVcl.Visual.RichLogUtils, LightVcl.Common.Colors, LightCore.AppData, LightVcl.Visual.AppData, LightVcl.Common.System;
+
+
+
+
+{--------------------------------------------------------------------------------------------------
+   FORM CONSTRUCTOR
+
+   The caller needs to call manually LoadForm(FrmUpdater).
+   This is necessary when I need to set the form as fsMDIChild. LoadForm needs to be called after fsMDIChild
+--------------------------------------------------------------------------------------------------}
+procedure TFrmUpdater.FormCreate(Sender: TObject);
+begin
+  Assert(Updater <> NIL);
+  Assert(lblVersion.Transparent= True);         { Needed for compatibility with VCL skins }
+
+  { Setup events }
+  Updater.OnUpdateStart  := OnUpdateStart;
+  Updater.OnNoNews       := OnNoNews;
+  Updater.OnHasNews      := OnHasNews;
+  Updater.OnConnectError := OnConnectError;
+  Updater.OnUpdateEnd    := OnUpdateEnd;
+
+  { GUI. Hide the link labels when the caller didn't supply a URL — matches FMX twin behavior
+    in FrameFMX\FormUpdaterNotifier and avoids showing broken clickable links to end users. }
+  lblDownload .Link      := Updater.URLDownload;
+  lblDownload .Visible   := Updater.URLDownload   <> '';
+  inetWhatsNew.Link      := Updater.URLRelHistory;
+  inetWhatsNew.Visible   := Updater.URLRelHistory <> '';
+  lblConnectError.Visible:= Updater.ConnectionError;
+
+  PopulateNews;
+end;
+
+
+procedure TFrmUpdater.FormDestroy(Sender: TObject);
+begin
+ { Disconnect updater from the form. Guard against the host app freeing Updater before
+   this form (shutdown-order hazard) — matches the FMX twin in FrameFMX\FormUpdaterNotifier. }
+ if Updater <> NIL then
+  begin
+   Updater.OnNoNews      := NIL;
+   Updater.OnHasNews     := NIL;
+   Updater.OnConnectError:= NIL;
+   Updater.OnUpdateStart := NIL;
+   Updater.OnUpdateEnd   := NIL;
+  end;
+end;
+
+
+procedure TFrmUpdater.FormClose(Sender: TObject; VAR Action: TCloseAction);
+begin
+  Action:= caFree;
+  //frmSettings.Container.Parent:= frmSettings;   { We need to move the container back on its original form, in order to let that form to correctly save its children }
+end;
+
+
+procedure TFrmUpdater.ShowDemoTab;
+begin
+  tabDemo.TabVisible:= TRUE;
+  tabRecEditor.TabVisible:= TRUE;
+  LogVerb.Visible:= TRUE;
+  LogVerb.Verbosity:= lvrVerbose;
+end;
+
+
+
+
+
+
+
+{--------------------------------------------------------------------------------------------------
+   MANUALY CHECK FOR NEWS
+--------------------------------------------------------------------------------------------------}
+procedure TFrmUpdater.btnCheckManuallyClick(Sender: TObject);
+begin
+ Log.Clear;
+ Log.AddVerb('Forced GetNews');
+
+ Updater.Delay:= 1; { Delay is ignored! }
+ btnCheckManually.Enabled:= FALSE;                 { Don't let user to close the form until the HTTP thread returns something (even an error). Else, I get an acces violation in THttpThread.Create }
+ LightVcl.Common.System.CursorBusy;                                       { Check for news }
+ TRY
+   Updater.GetNews;
+   PopulateNews;
+ FINALLY
+  btnCheckManually.Enabled:= TRUE;
+  CursorNotBusy;
+ END;
+end;
+
+
+
+{--------------------------------------------------------------------------------------------------
+   SHOW "NEW VERSION FOUND"
+--------------------------------------------------------------------------------------------------}
+
+procedure TFrmUpdater.PopulateNews;
+begin     //clr
+ { Show version }
+ lblVersion.Caption:= 'You are running version '+ TAppData.GetVersionInfo
+                +CRLF+'Online version is '+ Updater.NewsRec.AppVersion;
+
+ if Updater.NewsRec.CriticalUpd
+ then lblVersion.Caption:= lblVersion.Caption+ CRLF+ 'CRITICAL UPDATE!';
+
+ Log.AddMsg(Updater.NewsRec.NewsHeadline+ CRLF);
+ Log.AddMsg(Updater.NewsRec.NewsBody);
+
+ { Show new version in red }
+ if Updater.NewVersionFound(TAppData.GetVersionInfo) then
+  begin
+   { Show as label }
+   lblVersion.Color:= clRedFade;
+   lblVersion.StyleElements:= [];
+   lblVersion.Transparent:= FALSE;
+  end;
+end;
+
+
+{--------------------------------------------------------------------------------------------------
+   DEMO
+--------------------------------------------------------------------------------------------------}
+procedure TFrmUpdater.btnCheckTodayClick(Sender: TObject);
+begin
+ PageCtrl.ActivePage:= tabNews;
+
+ Log.Clear;
+ Log.AddVerb('CheckForNews Today');
+
+ Updater.Delay:= 1; { 1 seconds delay }
+ Updater.When:= cwHours;
+
+ if NOT Updater.IsTimeToCheckAgain
+ then Log.AddInfo('Already checked for news today.');
+
+ Updater.CheckForNews;
+end;
+
+
+procedure TFrmUpdater.btnIsTimeToCheckClick(Sender: TObject);
+begin
+ Log.Clear;
+ Log.AddInfo('Last succesful check for updates: '+ DateTimeToStr(Updater.LastUpdate));
+ PageCtrl.ActivePage:= tabNews;
+
+ if Updater.IsTimeToCheckAgain
+ then Log.AddInfo('IsTimeToCheckAgain: Yes')
+ else Log.AddInfo('IsTimeToCheckAgain: No');
+end;
+
+
+procedure TFrmUpdater.btnNewVersFoundClick(Sender: TObject);
+begin
+ if Updater.NewVersionFound(TAppData.GetVersionInfo)
+ then Log.AddInfo('NewVersionFound')
+ else Log.AddInfo('No NewVersionFound');
+
+ PageCtrl.ActivePage:= tabNews;
+end;
+
+
+procedure TFrmUpdater.btnCheckDelayClick(Sender: TObject);
+begin
+ Log.Clear;
+ Log.AddInfo('CheckForNews Delay: 3sec');
+ PageCtrl.ActivePage:= tabNews;
+
+ Updater.Delay:= 1; { 1 seconds delay }
+ Updater.When:= cwStartUp;
+ Updater.CheckForNews;
+end;
+
+
+
+
+
+
+{--------------------------------------------------------------------------------------------------
+   EVENTS
+--------------------------------------------------------------------------------------------------}
+procedure TFrmUpdater.OnUpdateEnd(Sender: TObject);
+begin
+ Log.AddVerb('End updater');
+end;
+
+
+procedure TFrmUpdater.OnUpdateStart(Sender: TObject);
+begin
+ lblConnectError.Visible:= FALSE;
+ lblStatus.Visible:= TRUE;
+ lblStatus.Transparent:= FALSE;
+ lblStatus.Color:= clOrange;                 { Background color (VCL Skins paint over Font.Color, so we tint the bkg) }
+ lblStatus.Caption:= 'Checking for news...';
+ lblStatus.Visible:= TRUE;
+ lblStatus.Refresh;                          { A blocking operation follows so we need to update this control to let user know that the update started }
+ Log.AddVerb(lblStatus.Caption);
+end;
+
+
+procedure TFrmUpdater.OnHasNews(Sender: TObject);
+begin
+ lblStatus.Visible:= TRUE;
+ lblStatus.Transparent:= FALSE;
+ lblStatus.Color:= clLimeDark;                     { Background color (lime) }
+ lblStatus.Font.Color:= clBlueNight;                { Font color (blue) — both set so VCL Skins don't repaint over us }
+ lblStatus.Caption:= 'We have news for you!';
+ Log.AddImpo('We have news for you!');
+
+ { Always show "new version available" if the user has an old version! }
+ PopulateNews;
+
+ if Updater.ForceNewsFound
+ then lblStatus.Caption:= lblStatus.Caption+ ' (BT: Forced)';
+
+ Assert(FormStyle<> fsMDIChild);
+
+ { Announce that we check for news so the user will know why the GUI freezes }
+ if Visible
+ then Show     { Cannot make a visible window modal! }
+ else ShowModal;
+end;
+
+
+
+procedure TFrmUpdater.OnNoNews(Sender: TObject);
+begin
+ lblStatus.Visible:= TRUE;
+ lblStatus.Transparent:= TRUE;
+ lblStatus.Caption:= 'No news today.';
+end;
+
+
+
+procedure TFrmUpdater.OnConnectError(Sender: TObject; Msg: string);
+begin
+ lblConnectError.Caption:= Msg;
+ lblConnectError.Visible:= TRUE;
+ lblStatus.Visible:= FALSE;
+ Log.AddError(Msg);
+
+ if Visible
+ then Show     { Cannot make a visible window modal! }
+ else ShowModal;
+end;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{--------------------------------------------------------------------------------------------------
+   GUI
+--------------------------------------------------------------------------------------------------}
+procedure TFrmUpdater.btnSettingsClick(Sender: TObject);
+begin
+  TfrmUpdaterSettings.CreateModal;
+end;
+
+
+procedure TFrmUpdater.btnBinFileClick(Sender: TObject);
+begin
+  TfrmRecEditor.CreateFormModal;   { Generate the BIN file that we upload on FTP }
+end;
+
+
+end.
---
+++ FrameVCL/FormUpdaterRecEditor.dfm
@@ -0,0 +1,337 @@
+object frmRecEditor: TfrmRecEditor
+  Left = 862
+  Top = 277
+  AlphaBlendValue = 245
+  Anchors = []
+  Caption = 'News & Updates Editor'
+  ClientHeight = 387
+  ClientWidth = 488
+  Color = clBtnFace
+  Constraints.MinHeight = 335
+  Constraints.MinWidth = 434
+  DoubleBuffered = True
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Arial'
+  Font.Style = []
+  Font.Quality = fqProof
+  Position = poDesigned
+  ScreenSnap = True
+  ShowHint = True
+  SnapBuffer = 3
+  OnClose = FormClose
+  OnDestroy = FormDestroy
+  TextHeight = 15
+  object pnlBtm: TPanel
+    Left = 0
+    Top = 351
+    Width = 488
+    Height = 36
+    Align = alBottom
+    TabOrder = 0
+    object btnSave: TButton
+      AlignWithMargins = True
+      Left = 100
+      Top = 4
+      Width = 90
+      Height = 28
+      Hint = 
+        'Close the window without saving.'#13#10'Hint: You can also close with ' +
+        'Escape.'
+      Align = alLeft
+      Caption = 'Save'
+      TabOrder = 0
+      OnClick = btnSaveClick
+    end
+    object btnLoad: TButton
+      AlignWithMargins = True
+      Left = 4
+      Top = 4
+      Width = 90
+      Height = 28
+      Hint = 
+        'Close the window without saving.'#13#10'Hint: You can also close with ' +
+        'Escape.'
+      Align = alLeft
+      Caption = 'Load'
+      TabOrder = 1
+      OnClick = btnLoadClick
+    end
+    object btnCopy: TButton
+      AlignWithMargins = True
+      Left = 196
+      Top = 6
+      Width = 57
+      Height = 24
+      Hint = 'Copy file path to clipboard'
+      Margins.Top = 5
+      Margins.Bottom = 5
+      Align = alLeft
+      Caption = 'Copy'
+      TabOrder = 2
+      OnClick = btnCopyClick
+    end
+    object btnClose: TButton
+      AlignWithMargins = True
+      Left = 394
+      Top = 4
+      Width = 90
+      Height = 28
+      Hint = 
+        'Close the window without saving.'#13#10'Hint: You can also close with ' +
+        'Escape.'
+      Align = alRight
+      Caption = 'Close'
+      ModalResult = 1
+      TabOrder = 3
+    end
+  end
+  object pnlTop: TPanel
+    AlignWithMargins = True
+    Left = 4
+    Top = 4
+    Width = 480
+    Height = 34
+    Margins.Left = 4
+    Margins.Top = 4
+    Margins.Right = 4
+    Margins.Bottom = 4
+    Align = alTop
+    TabOrder = 1
+    object lblCounter: TLabel
+      AlignWithMargins = True
+      Left = 4
+      Top = 4
+      Width = 44
+      Height = 29
+      Align = alLeft
+      Caption = 'NewsID'
+      Layout = tlCenter
+    end
+    object Label1: TLabel
+      AlignWithMargins = True
+      Left = 138
+      Top = 4
+      Width = 34
+      Height = 29
+      Align = alLeft
+      Caption = 'Target'
+      Layout = tlCenter
+    end
+    object Label2: TLabel
+      AlignWithMargins = True
+      Left = 297
+      Top = 4
+      Width = 34
+      Height = 29
+      Align = alLeft
+      Caption = 'Show '
+      Layout = tlCenter
+    end
+    object Label3: TLabel
+      AlignWithMargins = True
+      Left = 384
+      Top = 4
+      Width = 39
+      Height = 29
+      Align = alLeft
+      Caption = 'time(s)'
+      Layout = tlCenter
+    end
+    object spnCounter: TSpinEdit
+      AlignWithMargins = True
+      Left = 54
+      Top = 4
+      Width = 69
+      Height = 26
+      Hint = 
+        ' Increment it each time we publish new news. This is how the pro' +
+        'gram detects that a new announcement was published online.'
+      Align = alLeft
+      MaxValue = 1000000
+      MinValue = 1
+      TabOrder = 0
+      Value = 1
+    end
+    object cmbTarget: TComboBox
+      AlignWithMargins = True
+      Left = 178
+      Top = 6
+      Width = 104
+      Height = 23
+      Margins.Top = 5
+      Align = alLeft
+      TabOrder = 1
+      Text = 'cmbTarget'
+    end
+    object Panel3: TPanel
+      AlignWithMargins = True
+      Left = 288
+      Top = 4
+      Width = 3
+      Height = 26
+      Align = alLeft
+      TabOrder = 2
+    end
+    object Panel4: TPanel
+      AlignWithMargins = True
+      Left = 129
+      Top = 4
+      Width = 3
+      Height = 26
+      Align = alLeft
+      TabOrder = 3
+    end
+    object spnShowCntr: TSpinEdit
+      AlignWithMargins = True
+      Left = 337
+      Top = 4
+      Width = 41
+      Height = 26
+      Hint = 'How many times to show this to the user. Usually only one time.'
+      Align = alLeft
+      MaxValue = 1000000
+      MinValue = 1
+      TabOrder = 4
+      Value = 1
+    end
+  end
+  object Panel5: TPanel
+    AlignWithMargins = True
+    Left = 4
+    Top = 46
+    Width = 480
+    Height = 34
+    Margins.Left = 4
+    Margins.Top = 4
+    Margins.Right = 4
+    Margins.Bottom = 4
+    Align = alTop
+    TabOrder = 2
+    object lblVers: TLabel
+      AlignWithMargins = True
+      Left = 4
+      Top = 4
+      Width = 44
+      Height = 29
+      Align = alLeft
+      Caption = 'Version:'
+      Layout = tlCenter
+    end
+    object Panel7: TPanel
+      AlignWithMargins = True
+      Left = 198
+      Top = 4
+      Width = 3
+      Height = 26
+      Align = alLeft
+      TabOrder = 0
+    end
+    object Panel8: TPanel
+      AlignWithMargins = True
+      Left = 340
+      Top = 4
+      Width = 3
+      Height = 26
+      Align = alLeft
+      TabOrder = 1
+    end
+    object edtOnlineVer: TEdit
+      AlignWithMargins = True
+      Left = 54
+      Top = 6
+      Width = 138
+      Height = 23
+      Hint = 'Online version, no build number.'#13#10'Ex: 14.4.0'#13#10
+      Margins.Top = 5
+      Margins.Bottom = 4
+      Align = alLeft
+      TabOrder = 2
+      TextHint = 'Online version (14.4.0)'
+    end
+    object chkCriticalUpd: TCheckBox
+      AlignWithMargins = True
+      Left = 349
+      Top = 4
+      Width = 123
+      Height = 26
+      Align = alLeft
+      Caption = 'Critical update'
+      TabOrder = 3
+    end
+    object chkBetaVer: TCheckBox
+      AlignWithMargins = True
+      Left = 207
+      Top = 4
+      Width = 127
+      Height = 26
+      Align = alLeft
+      Caption = 'Is beta version'
+      TabOrder = 4
+    end
+  end
+  object GroupBox1: TGroupBox
+    AlignWithMargins = True
+    Left = 4
+    Top = 88
+    Width = 480
+    Height = 226
+    Margins.Left = 4
+    Margins.Top = 4
+    Margins.Right = 4
+    Margins.Bottom = 4
+    Align = alClient
+    Caption = 'News'
+    TabOrder = 3
+    object edtHeadline: TEdit
+      AlignWithMargins = True
+      Left = 5
+      Top = 20
+      Width = 470
+      Height = 23
+      Hint = 'News headline'
+      Align = alTop
+      TabOrder = 0
+      TextHint = 'Text headline'
+    end
+    object Memo: TMemo
+      AlignWithMargins = True
+      Left = 8
+      Top = 52
+      Width = 464
+      Height = 166
+      Margins.Left = 6
+      Margins.Top = 6
+      Margins.Right = 6
+      Margins.Bottom = 6
+      TabStop = False
+      Align = alClient
+      Font.Charset = ANSI_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -12
+      Font.Name = 'Arial'
+      Font.Style = []
+      Font.Quality = fqProof
+      HideSelection = False
+      MaxLength = 2147483632
+      ParentFont = False
+      ScrollBars = ssHorizontal
+      TabOrder = 1
+      WordWrap = False
+    end
+  end
+  object edtComment: TEdit
+    AlignWithMargins = True
+    Left = 3
+    Top = 323
+    Width = 482
+    Height = 24
+    Hint = 'Internal comment'
+    Margins.Top = 5
+    Margins.Bottom = 4
+    Align = alBottom
+    TabOrder = 4
+    TextHint = 'Comments'
+  end
+end
