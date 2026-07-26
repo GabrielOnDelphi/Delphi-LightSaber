0cf6dc8560c33c4a2f75ea3f4b5a1c81862adae0
Gabriel Moraru <cubicDesign@gmail.com>
2026-07-26 16:20:42 +0200

    tests

--- External/UnitTesting/BuildTests.cmd
+++ External/UnitTesting/BuildTests.cmd
@@ -1,4 +1,4 @@
 @echo off
 call "c:\Delphi\Delphi 13\bin\rsvars.bat"
 REM Build without TESTINSIGHT to enable console output
-"c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "Tests.dproj" /t:Build /p:platform=Win32 /p:Configuration=Debug /p:DCC_Define="DEBUG"
+"c:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe" "Tests.dproj" /t:Build /p:platform=Win32 /p:Config=Debug /p:DCC_Define="DEBUG"
